2bd06615fc321ce33835cfe19de5adbb1ba1544c
Enis Hrnjic <ehrnjic@gmail.com>
2020-02-19 23:36:18 +0100

    Added: ALL on/off & STATUS cmnds

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ var net = require('net')
 const conf = require('config').get('configuration')
 
 // Init mqtt and net clients
-var mqtt_client  = mqtt.connect(conf.mqtt.server)
-var tcp_client = new net.Socket() 
+var mqtt_client = mqtt.connect(conf.mqtt.server)
+var tcp_client = new net.Socket()
 
-const cmndPwrTempl = [0xaa, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xbb]
+const sendPktTepl = [0xaa, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0b, 0xbb]
+
 
 // MQTT event handler CONNECT
 mqtt_client.on('connect', function () {
   mqtt_client.subscribe(conf.mqtt.subCommandTopic, function (err) {
     if (!err) {
       console.log('Connected to MQTT server: ' + conf.mqtt.server
-       + ' and subscribed to topic: ' + conf.mqtt.subCommandTopic)
+        + ' and subscribed to topic: ' + conf.mqtt.subCommandTopic)
     }
   })
 })
@@ -26,29 +27,65 @@ mqtt_client.on('connect', function () {
 // MQTT event handler MESSAGE
 mqtt_client.on('message', function (topic, message) {
   console.log('Received message: ' + message + ' on topic: ' + topic)
-  
-  var stringMessage = message.toString()
-  // If msg start with PWR that mean POWER command
-  if (stringMessage.substr(0,3) == 'PWR') {
-    var netPacket = Buffer.from(cmndPwrTempl)
 
-    // Now get 5-th char of msg - 0 = off, 1 = on
-    if (stringMessage.substr(4,1) == '0') {
+  var stringMessage = message.toString()
+
+  // If msg start with PWR that mean POWER (0x10) command
+  var netPacket = Buffer.from(sendPktTepl)
+  if (stringMessage.substr(0, 3) == 'PWR') {
+    netPacket[1] = 0x0f
+
+    // Now get 5-th char oon
+    if (stringMessage.substr(4, 1) == '0') {
       netPacket[3] = 0x02
-    } else if (stringMessage.substr(4,1) == '1') {
+    } else if (stringMessage.substr(4, 1) == '1') {
       netPacket[3] = 0x01
     }
 
     // Now get 7&8-th char of msg - 00 to 15 for relay 0 to 15
-    netPacket[2] = stringMessage.substr(6,2).toString(16)
-    
+    netPacket[2] = stringMessage.substr(6, 2).toString(16)
+
     // Send cmnd via tcp to board
+    tcp_client.write(netPacket)
+    console.log('Send to board via tcp: ' + netPacket.toString('hex'))
+  }
+
+  // If msg start with ALL that mean POWER ON ALL (param=1) or OFF (param=0)
+  if (stringMessage.substr(0, 3) == "ALL") {
+    if (stringMessage.substr(4, 1) == "1") {
+      netPacket[1] = 0x0a
+      netPacket[2] = 0x03
+      tcp_client.write(netPacket)
+      console.log('Send to board via tcp :' + netPacket.toString('hex'))
+    } else if (stringMessage.substr(4, 1) == "0") {
+      netPacket[1] = 0x0b
+      tcp_client.write(netPacket)
+      console.log('Send to board via tcp :' + netPacket.toString('hex'))
+    }
+  }
+
+  if (stringMessage.substr(0, 3) == "STA") {
+    netPacket[1] = 0x1c
     tcp_client.write(netPacket)
     console.log('Send to board via tcp :' + netPacket.toString('hex'))
   }
 })
 
-tcp_client.connect(conf.board.tcpPort, conf.board.ipAddr, function() {
-    console.log('Connected to relay board: ' + conf.board.ipAddr + ' tcp' + conf.board.tcpPort
+
+
+// TCP event handler CONNECT
+tcp_client.connect(conf.board.tcpPort, conf.board.ipAddr, function () {
+  console.log('Connected to relay board: ' + conf.board.ipAddr + ' tcp' + conf.board.tcpPort
   )
+})
+
+// TCP event handler DATA
+tcp_client.on('data', function (data) {
+  console.log('Received data from board: ' + data.toString('hex'))
+  var checksum = 0
+  for (i=1; i<18; i++) {
+    checksum += data[i]
+  }
+  console.log('Received data checksum: ' + checksum.toString(16) + ' is ' 
+    + data[18].toString(16))
 })
